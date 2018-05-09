6fb3d736a73b15e021ca9d8e5a18bd491ba6794a
Stefan Wille <github@stefanwille.com>
2018-05-09 20:50:04 +0200

    Make the spec pop more

--- src/demo.spec.js
+++ src/demo.spec.js
@@ -2,25 +2,25 @@ const produce = require("immer").default;
 
 describe("immer.produce", () => {
   const oldState = {
-    a: 1,
+    a: "old",
     b: [4, 5, 6]
   };
   const newState = produce(oldState, draftState => {
-    draftState.a = 2;
-    draftState.b[1] = 9;
+    draftState.a = "new";
+    draftState.b[1] = 999;
   });
 
   it("returns a new state based on the old state and a modified draft state", () => {
     const expectedState = {
-      a: 2,
-      b: [4, 9, 6]
+      a: "new",
+      b: [4, 999, 6]
     };
     expect(newState).toEqual(expectedState);
   });
 
   it("leaves the old state unmodified", () => {
     const expectedState = {
-      a: 1,
+      a: "old",
       b: [4, 5, 6]
     };
     expect(oldState).toEqual(expectedState);
