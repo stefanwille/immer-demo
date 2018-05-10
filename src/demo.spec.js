5f689153fb37fb6a8651940cbe7460b1ac861158
Stefan Wille <github@stefanwille.com>
2018-05-10 11:34:27 +0200

    Be more expressive

--- src/demo.spec.js
+++ src/demo.spec.js
@@ -1,28 +1,34 @@
 const produce = require("immer").default;
 
-describe("immer.produce", () => {
-  const oldState = {
-    a: "old",
-    b: [4, 5, 6]
-  };
-  const newState = produce(oldState, draftState => {
-    draftState.a = "new";
-    draftState.b[1] = 999;
-  });
-
+describe("immer#produce", () => {
   it("returns a new state based on the old state and a modified draft state", () => {
-    const expectedState = {
-      a: "new",
-      b: [4, 999, 6]
-    };
-    expect(newState).toEqual(expectedState);
-  });
-
-  it("leaves the old state unmodified", () => {
-    const expectedState = {
+    const oldState = {
       a: "old",
       b: [4, 5, 6]
     };
-    expect(oldState).toEqual(expectedState);
+    const newState = produce(oldState, draftState => {
+      draftState.a = "new";
+      draftState.b[1] = 999;
+    });
+
+    expect(newState).toEqual({
+      a: "new",
+      b: [4, 999, 6]
+    });
+  });
+
+  it("leaves the old state unmodified", () => {
+    const oldState = {
+      a: "old",
+      b: [4, 5, 6]
+    };
+    produce(oldState, draftState => {
+      draftState.a = "new";
+      draftState.b[1] = 999;
+    });
+    expect(oldState).toEqual({
+      a: "old",
+      b: [4, 5, 6]
+    });
   });
 });
